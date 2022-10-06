a06493b600e3031ebbe3dd143c627518cc6fc234
z1nk <t.zink.business@outlook.com>
2022-10-06 15:26:16 +0200

    solved shiny-app-io exercise

---
+++ exercises/lecture-exercises/lecture05/in-class-exercise/app.R
@@ -0,0 +1,51 @@
+library(shiny)
+library(tidyverse)
+
+#####Import Data
+
+#type in your data path
+dat <- read_csv("DataExerciseShinyApps.csv")
+dat <- dat %>% select(c("pid7", "ideo5"))
+
+#remove missing values 
+dat <- drop_na(dat)
+
+# Define UI for application that draws a figure
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel(""),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("five_point_ideology",
+                  "Select Five Point Ideology (1=Very liberal, 5=Very conservative):",
+                  min = 1,
+                  max = 5,
+                  value = 3)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("ideology_plot")
+    )
+  )
+)
+  
+server <- function(input, output) {
+  
+  output$ideology_plot <- renderPlot({
+    
+    ggplot(
+      filter(dat, ideo5 == input$five_point_ideology), 
+      aes(x = factor(pid7))) +
+      geom_bar(color = "black") +
+      scale_x_discrete(breaks = dat$pid7, labels = dat$pid7) +
+      xlab("7 Point Party ID, 1 = very D, 7 = very R") +
+      ylab("Count")
+  })
+}
+
+# Run the application 
+shinyApp(ui, server)
