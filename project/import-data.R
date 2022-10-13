07ea24acc0028007a7cbcbafa5b6931eef8c9afe
z1nk <t.zink.business@outlook.com>
2022-10-13 12:29:10 +0200

    bar chart regarding number of times used for some champions

--- project/import-data.R
+++ project/import-data.R
@@ -3,12 +3,41 @@
 setwd("project")
 
 # libraries
-library(rjson)
 library(dplyr)
+library(ggplot2)
+library(tidyverse)
 
 # loading the data
-json_file <- "data.json"
-json_data <- fromJSON(file = json_file)
-json_data_frame <- as.data.frame(json_data$SummonerIds$kills)
+data <- read.csv("selected-data.csv", sep = ",")
+data_frame <- as.data.frame(data)
+data_frame[1, ]
 
-json_data_frame[1:10,]
+data_frame_reduced <- filter(data_frame, championName == "Draven" |
+                                         championName == "Jinx" |
+                                         championName == "Kai'Sa" |
+                                         championName == "Lucian" |
+                                         championName == "Miss Fortune" |
+                                         championName == "Sivir" |
+                                         championName == "Tristana" |
+                                         championName == "Twitch" |
+                                         championName == "Varus" |
+                                         championName == "Vayne" |
+                                         championName == "Xayah" |
+                                         championName == "Ziggs")
+
+# plotting gameDuration to each gameID
+plot(data_frame$gameId, data_frame$gameDuration,
+        type = "l",
+        xlab = "Game ID",
+        ylab = "Game Duration",
+        main = "Game Duration to Game ID")
+
+# plotting number of times champions have been used
+ggplot(
+      data_frame_reduced,
+      aes(x = championName)) +
+      geom_bar(mapping = aes(championName),
+                     color = "black",
+                     fill = "gray") +
+      xlab("ChampionName") +
+      ylab("Count")
