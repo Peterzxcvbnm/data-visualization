f3b03a0198c5c1fe33b92d9a5dec598a519d2a8a
Kristian <krust@student.sdu.dk>
2022-10-30 17:00:25 +0100

    Test plot for Death vs VisionScore

---
+++ project/visionscore-to-deaths.R
@@ -0,0 +1,39 @@
+# setting working directory
+setwd("project")
+
+httpgd::hgd()
+httpgd::hgd_browse()
+
+# libraries
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+
+# loading the data
+data <- read.csv("selected-data.csv", sep = ",")
+data_frame <- as.data.frame(data)
+data_frame[1, ]
+
+data_frame_deaths = data_frame %>%
+      group_by(gameId, teamId) %>%
+      summarise(visionScore = mean(visionScore), deaths = mean(deaths)) %>%
+      group_by(gameId) %>%
+      summarise(visionScore = diff(visionScore), deaths = first(deaths)) %>%
+      mutate(visionScoreDiff = cut(visionScore,
+                                   breaks = c(-Inf, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, Inf),
+                                   right = TRUE)) %>%
+      group_by(visionScoreDiff) %>%
+      summarise(deaths = mean(deaths))
+      
+
+
+print(data_frame_deaths)
+
+plot(data_frame_deaths$visionScoreDiff, data_frame_deaths$deaths,
+        type = "l",
+        xlab = "visionScoreDiff",
+        ylab = "Deaths",
+        main = "Deaths by vision score diff")
+
+# plotting number of times champions have been used
+
