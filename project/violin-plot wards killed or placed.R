ae2afad04e3c4871f905c6459e4b51882f827936
Peterzxcvbnm <peter.braendgaard@gmail.com>
2022-11-14 13:41:48 +0100

    bidir barchart

--- project/violin-plot wards killed or placed.R
+++ project/violin-plot wards killed or placed.R
@@ -13,14 +13,17 @@ library(tidyverse)
 data <- read.csv("selected-data.csv", sep = ",")
 data_frame <- as.data.frame(data)
 
+data_frame = data_frame %>%
+    filter(teamPosition != "")
+data_frame$teamPosition = factor(data_frame$teamPosition, levels = c("TOP", "JUNGLE", "MIDDLE", "BOTTOM", "UTILITY"))
 
-plot = ggplot(data_frame, aes(x = win, y = wardsKilled)) +
+plot = ggplot(data_frame, aes(x = teamPosition, y = wardsKilled, fill=win)) +
     geom_violin(position = position_dodge(1)) +
     labs(title = "Wards killed", x = "Wards killed", y = "Count") + # nolint
     theme(plot.title = element_text(hjust = 0.5)) +
     geom_boxplot(width=0.25, position = position_dodge(1))
 
-plot2 = ggplot(data_frame, aes(x = win, y = wardsPlaced)) +
+plot2 = ggplot(data_frame, aes(x = teamPosition, y = wardsPlaced, fill=win)) +
     geom_violin(position = position_dodge(1)) +
     labs(title = "Wards placed", x = "Wards placed", y = "Count") + # nolint
     theme(plot.title = element_text(hjust = 0.5)) +
