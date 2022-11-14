bf5b17e8d6586c6678a227211b01f98749bf7792
Omvk97 <olvan18@student.sdu.dk>
2022-11-14 20:24:35 +0100

    Champion winrate for each lane

---
+++ project/bestChampionPick.R
@@ -0,0 +1,4 @@
+
+championVariable = "Ahri"
+
+# create data that finds all the games with ahri. Find championName on her opponents and whether or not she won
--- project/championWinRate.R
+++ project/championWinRate.R
@@ -1,28 +1,42 @@
 # setting working directory
 setwd("project")
-
+install.packages("shadowtext")
 # libraries
-library(ggplot2)
-library(dplyr)
-library(plotly)
-library(hrbrthemes)
-library(GGally)
-library(dplyr)
+library(grid)
+library(tidyverse)
+library(shadowtext)
+
 
 # loading the data
 data <- read.csv("/Users/oliver/OfflineDocuments/git/Uddannelse/data-visualization/project/selected-data.csv", sep = ",")
 df <- as.data.frame(data)
-df <- subset(df, select = c("championName", "win", "gameDuration"))
+df <- subset(df, select = c("championName", "win", "gameDuration", "teamPosition"))
+df <- df[df$gameDuration >= 240,] # outliers. The ones ending in a draw
+
 df$win <- as.logical(df$win)
-summary(df)
+topDf <- df[df$teamPosition == "TOP",]
+jungleDf <- df[df$teamPosition == "JUNGLE",]
+middleDf <- df[df$teamPosition == "MIDDLE",]
+bottomDf <- df[df$teamPosition == "BOTTOM",]
+supportDf <- df[df$teamPosition == "UTILITY",]
 
-df_grouped = df %>% group_by(championName) %>% summarise(wins = sum(win, na.rm = TRUE), totalGames = n(), winRate = sum(win, na.rm = TRUE) / n())
-df_grouped <- subset(df_grouped, select = c("championName", "winRate"))
+make_lane_graph <- function(df, color) {
+  df_grouped = df %>% group_by(championName) %>% filter(n() > 9) %>% summarise(wins = sum(win, na.rm = TRUE), totalGames = n(), winRate = sum(win, na.rm = TRUE) / n())
+  df_grouped = df_grouped[order(df_grouped$winRate), ]
+  
+  data <- data.frame(
+    winRate = df_grouped$winRate,
+    name = factor(df_grouped$championName, levels = df_grouped$championName)
+  )
+  
+  plt <- ggplotly(ggplot(data) +
+    geom_col(aes(winRate, name), fill = color, width = 0.5))
+  
+  plt
+}
 
-df_grouped <- df_grouped[order(df_grouped$winRate),]
-
-p <- ggplot(df_grouped, aes(x = reorder(championName, -winRate), y = winRate)) +
-  geom_bar(stat='identity', fill = "#FF6666")
-
-p <- ggplotly(p)
-p
+make_lane_graph(topDf, "#1338BE")
+make_lane_graph(jungleDf, "#048243")
+make_lane_graph(middleDf, "#C21807")
+make_lane_graph(bottomDf, "#5a3d46")
+make_lane_graph(supportDf, "#FF8300")
