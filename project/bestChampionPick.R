9a306347f2080863c33865bd675a8fbd029adce9
Omvk97 <olvan18@student.sdu.dk>
2022-11-21 12:15:44 +0100

    BestChampionPick

--- project/bestChampionPick.R
+++ project/bestChampionPick.R
@@ -1,4 +1,30 @@
+install.packages("ggplotly")
+library(tidyverse)
+library(ggplot2)
+library(grid)
+library(shadowtext)
 
 championVariable = "Ahri"
+teamPositionVariable = "MIDDLE"
 
-# create data that finds all the games with ahri. Find championName on her opponents and whether or not she won
+data <- read.csv("/Users/oliver/OfflineDocuments/git/Uddannelse/data-visualization/project/selected-data.csv", sep = ",")
+df <- as.data.frame(data)
+df <- df[df$gameDuration >= 240,] # outliers. The ones ending in a draw
+df$win <- as.logical(df$win)
+
+df_grouped <- df %>% group_by(gameId) %>% filter(any(championName==championVariable)) %>% filter(teamPosition == teamPositionVariable)
+df_grouped <- df_grouped[df_grouped$championName != "Ahri",]
+df_grouped <- df_grouped %>% ungroup() %>% group_by(championName) %>% summarise(wins = sum(win, na.rm = TRUE), totalGames = n(), winRate = 1- (sum(win, na.rm = TRUE) / n()))
+
+df_grouped = df_grouped[order(df_grouped$winRate), ]
+
+data <- data.frame(
+  winRate = df_grouped$winRate,
+  name = factor(df_grouped$championName, levels = df_grouped$championName)
+)
+
+
+plt <- ggplot(data) +
+                  geom_col(aes(winRate, name), width = 0.5)
+
+plt
