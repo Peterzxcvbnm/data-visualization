f08c86fc690fefb7000a9e276cd60a497f5ebcfd
z1nk <t.zink.business@outlook.com>
2022-11-24 14:27:29 +0100

    observe event with animation works as intended - ui has been altered

--- project/shiny_app/ui.R
+++ project/shiny_app/ui.R
@@ -4,11 +4,13 @@ library(shiny)
 ui <- fluidPage(
 
 # Application title
-  titlePanel(""),
+  titlePanel("Data Visualization - Group 12 - League of Legends"),
 
-# Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
+# Sidebar with a slider input for number of bins
+  fluidRow(
+    column(6,
+      h3("Role and champion selection"),
+      h5("Select role and champion to find out which champion is the superior one."),
       selectInput("positions_selected",
                   "Select positions:",
                   c("TOP", "JUNGLE", "MIDDLE", "BOTTOM", "UTILITY"),
@@ -22,8 +24,13 @@ ui <- fluidPage(
       textInput("champion_pick",
                 "Champion:",
                 "Aatrox"),
+    ),
+    
+    column(6,
+      h3("Animation"),
+      h5("The animation plot can show any of the variables below over time. This will show differences between victories and defeats."),
       selectInput("animation_y_value",
-                  "Stat:",
+                  "Variable:",
                   c("assists", "deaths", "goldEarned", "kills", "totalDamageDealt", "visionScore", "wardsKilled", "wardsPlaced", "totalMinionsKilled"),
                   multiple = FALSE,
                   selected = c("assists")),
@@ -33,32 +40,43 @@ ui <- fluidPage(
                   max = 600,
                   value = 120,
                   step = 30),
-       actionButton(
+      actionButton(
         inputId = "submit_animation",
         label = "Submit animation properties"
       )
-    ),
+    )
+  ),
+
+  hr(),
 
 # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("violin_kills_assists_diff"),
-      plotOutput("vision_score_distribution"),
-      plotOutput("wards_placed_by_position"),
-      plotOutput("wards_killed_by_position"),
-      plotOutput("vision_score_by_wins"),
-      plotOutput("best_champion_pick"),
-      plotOutput("top_champion_winrate"),
-      plotOutput("jungle_champion_winrate"),
-      plotOutput("middle_champion_winrate"),
-      plotOutput("bottom_champion_winrate"),
-      plotOutput("utility_champion_winrate"),
-      plotOutput("vision_score_by_deaths"),
-      plotOutput("gameDuration_to_visionScore"),
-      plotOutput("gameDuration_to_deaths"),
-      plotOutput("teamid_teamposition_win"),
-      plotOutput("wards_Killed_violinPlot"),
-      plotOutput("wards_Placed_violinPlot"),
-      plotOutput("animation_plot")
-    )
-  )
+  plotOutput("violin_kills_assists_diff"),
+
+  fluidRow(
+    column(6, plotOutput("vision_score_distribution")),
+    column(6, plotOutput("vision_score_by_wins"))
+  ),
+
+  fluidRow(
+    column(6, plotOutput("wards_placed_by_position")),
+    column(6, plotOutput("wards_killed_by_position"))
+  ),
+
+  plotOutput("best_champion_pick"),
+  plotOutput("top_champion_winrate"),
+  plotOutput("jungle_champion_winrate"),
+  plotOutput("middle_champion_winrate"),
+  plotOutput("bottom_champion_winrate"),
+  plotOutput("utility_champion_winrate"),
+
+  fluidRow(
+    column(4, plotOutput("vision_score_by_deaths")),
+    column(4, plotOutput("gameDuration_to_visionScore")),
+    column(4, plotOutput("gameDuration_to_deaths"))
+  ),
+
+  plotOutput("teamid_teamposition_win"),
+  plotOutput("wards_Killed_violinPlot"),
+  plotOutput("wards_Placed_violinPlot"),
+  plotOutput("animation_plot")
 )
