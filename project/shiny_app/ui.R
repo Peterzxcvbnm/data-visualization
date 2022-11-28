75e1d070b58ddfa5d2f96c016378b03f9fdae96a
Peterzxcvbnm <peter.braendgaard@gmail.com>
2022-11-28 14:40:11 +0100

    Better ui

--- project/shiny_app/ui.R
+++ project/shiny_app/ui.R
@@ -1,82 +1,136 @@
 # libraries
 library(shiny)
-# Define UI for application that draws a figure
-ui <- fluidPage(
+library(shinydashboard)
 
-# Application title
-  titlePanel("Data Visualization - Group 12 - League of Legends"),
+introductionPage = fluidPage(titlePanel("Introduction"),
+                             h3("Stuff")
+                             )
 
-# Sidebar with a slider input for number of bins
-  fluidRow(
-    column(6,
-      h3("Role and champion selection"),
-      h5("Select role and champion to find out which champion is the superior one."),
-      selectInput("positions_selected",
-                  "Select positions:",
-                  c("TOP", "JUNGLE", "MIDDLE", "BOTTOM", "UTILITY"),
-                  multiple = TRUE,
-                  selected = c("TOP", "JUNGLE", "MIDDLE", "BOTTOM", "UTILITY")),
-      selectInput("champion_pick_lane",
-                  "Lane:",
-                  c("TOP", "JUNGLE", "MIDDLE", "BOTTOM", "UTILITY"),
-                  multiple = FALSE,
-                  selected = c("TOP")),
-      textInput("champion_pick",
-                "Champion:",
-                "Aatrox"),
+visionPage = fluidPage(# App title ----
+                       titlePanel("Vision"),
+                       
+                       # Sidebar layout with input and output definitions ----
+                       sidebarLayout(# Sidebar panel for inputs ----
+                                     sidebarPanel(
+                                       selectInput(
+                                         "positions_selected",
+                                         "Select positions:",
+                                         c("TOP", "JUNGLE", "MIDDLE", "BOTTOM", "UTILITY"),
+                                         multiple = TRUE,
+                                         selected = c("TOP", "JUNGLE", "MIDDLE", "BOTTOM", "UTILITY")
+                                       ),
+                                     ),
+                                     
+                                     # Main panel for displaying outputs ----
+                                     mainPanel(
+                                       # Output: Tabset w/ plot, summary, and table ----
+                                       tabsetPanel(
+                                         type = "tabs",
+                                         tabPanel("Overall", fluidRow(
+                                           column(6, plotOutput("vision_score_distribution")),
+                                           column(6, plotOutput("vision_score_by_wins"))
+                                         )),
+                                         tabPanel("Wards", fluidRow(
+                                           column(6, plotOutput("wards_placed_by_position")),
+                                           column(6, plotOutput("wards_killed_by_position"))
+                                         )),
+                                         tabPanel("Deaths", fluidRow(
+                                           column(4, plotOutput("vision_score_by_deaths")),
+                                           column(4, plotOutput("gameDuration_to_visionScore")),
+                                           column(4, plotOutput("gameDuration_to_deaths"))
+                                         )),
+                                         tabPanel("Violin", fluidPage(
+                                           plotOutput("wards_Killed_violinPlot"),
+                                           plotOutput("wards_Placed_violinPlot")
+                                         ))
+                                       )
+                                       
+                                     )))
+
+championsPage = fluidPage(titlePanel("Champions"),
+                          
+                          sidebarLayout(
+                            sidebarPanel(
+                              selectInput(
+                                "champion_pick_lane",
+                                "Lane:",
+                                c("TOP", "JUNGLE", "MIDDLE", "BOTTOM", "UTILITY"),
+                                multiple = FALSE,
+                                selected = c("TOP")
+                              ),
+                              textInput("champion_pick",
+                                        "Champion:",
+                                        "Aatrox")
+                            ),
+                            
+                            mainPanel(
+                              tabsetPanel(
+                                type = "tabs",
+                                tabPanel("Best champion", plotOutput("best_champion_pick")),
+                                tabPanel("Top", plotOutput("top_champion_pick")),
+                                tabPanel("Jungle", plotOutput("jungle_champion_pick")),
+                                tabPanel("Middle", plotOutput("middle_champion_pick")),
+                                tabPanel("Bottom", plotOutput("bottom_champion_pick")),
+                                tabPanel("Utility", plotOutput("utility_champion_pick"))
+                              )
+                            )
+                          ))
+
+animationPage = fluidPage(
+  titlePanel("Animation"),
+  h5(
+    "The animation plot can show any of the variables below over time. This will show differences between victories and defeats."
+  ),
+  selectInput(
+    "animation_y_value",
+    "Variable:",
+    c(
+      "assists",
+      "deaths",
+      "goldEarned",
+      "kills",
+      "totalDamageDealt",
+      "visionScore",
+      "wardsKilled",
+      "wardsPlaced",
+      "totalMinionsKilled"
     ),
-    
-    column(6,
-      h3("Animation"),
-      h5("The animation plot can show any of the variables below over time. This will show differences between victories and defeats."),
-      selectInput("animation_y_value",
-                  "Variable:",
-                  c("assists", "deaths", "goldEarned", "kills", "totalDamageDealt", "visionScore", "wardsKilled", "wardsPlaced", "totalMinionsKilled"),
-                  multiple = FALSE,
-                  selected = c("assists")),
-      sliderInput("animation_length_of_intervals",
-                  "Length of intervals [s]:",
-                  min = 60,
-                  max = 600,
-                  value = 120,
-                  step = 30),
-      actionButton(
-        inputId = "submit_animation",
-        label = "Submit animation properties"
-      )
+    multiple = FALSE,
+    selected = c("assists")
+  ),
+  sliderInput(
+    "animation_length_of_intervals",
+    "Length of intervals [s]:",
+    min = 60,
+    max = 600,
+    value = 120,
+    step = 30
+  ),
+  actionButton(inputId = "submit_animation",
+               label = "Submit animation properties"),
+  plotOutput("animation_plot")
+)
+
+# Define UI for application that draws a figure
+ui <- dashboardPage(
+  # Application title
+  dashboardHeader(title = "Data Visualization - Group 12 - League of Legends"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Introduction", tabName = "intro", icon = icon("info")),
+      menuItem("Vision", tabName = "vision", icon = icon("eye")),
+      menuItem("Champions", tabName = "champions", icon = icon("user")),
+      menuItem("Animation", tabName = "animation", icon = icon("film")),
+      menuItem("Conclusion", tabName = "conclusion", icon = icon("info"))
     )
   ),
-
-  hr(),
-
-# Show a plot of the generated distribution
-  plotOutput("violin_kills_assists_diff"),
-
-  fluidRow(
-    column(6, plotOutput("vision_score_distribution")),
-    column(6, plotOutput("vision_score_by_wins"))
-  ),
-
-  fluidRow(
-    column(6, plotOutput("wards_placed_by_position")),
-    column(6, plotOutput("wards_killed_by_position"))
-  ),
-
-  plotOutput("best_champion_pick"),
-  plotOutput("top_champion_winrate"),
-  plotOutput("jungle_champion_winrate"),
-  plotOutput("middle_champion_winrate"),
-  plotOutput("bottom_champion_winrate"),
-  plotOutput("utility_champion_winrate"),
-
-  fluidRow(
-    column(4, plotOutput("vision_score_by_deaths")),
-    column(4, plotOutput("gameDuration_to_visionScore")),
-    column(4, plotOutput("gameDuration_to_deaths"))
-  ),
-
-  plotOutput("teamid_teamposition_win"),
-  plotOutput("wards_Killed_violinPlot"),
-  plotOutput("wards_Placed_violinPlot"),
-  plotOutput("animation_plot")
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "intro", introductionPage),
+    tabItem(tabName = "vision", visionPage),
+    tabItem(tabName = "champions", championsPage),
+    tabItem(tabName = "animation", animationPage),
+    tabItem(tabName = "conclusion", h3("Conclusion"))
+    )
+  )
 )
